642fb5f60f623a30080537065fd4a87a3dd62a0c
HmViper <mazurink@mail.ru>
2021-09-10 16:13:43 +0300

    3. Реализация профессий в коде [#6837]

---
+++ src/main/java/ru/job4j/inheritance/Task.java
@@ -0,0 +1,13 @@
+package ru.job4j.inheritance;
+
+public class Task {
+    private int isTaskComplete;
+
+    public void setIsTaskComplete(int isTaskComplete) {
+        this.isTaskComplete = isTaskComplete;
+    }
+
+    public int getIsTaskComplete() {
+        return isTaskComplete;
+    }
+}
