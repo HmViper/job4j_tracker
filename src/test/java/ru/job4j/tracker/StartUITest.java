123d54c28f1895aed9d35f35183022e331aa6649
Cyrill <mazurink@mail.ru>
2021-09-22 18:51:49 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #206488]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -35,10 +35,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
         tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "1"
-        };
+        String[] answers = {String.valueOf(item.getId())};
         StartUI.deleteItem(new StubInput(answers), tracker);
         assertNull(tracker.findById(item.getId()));
     }
