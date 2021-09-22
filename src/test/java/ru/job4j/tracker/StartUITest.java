ea24c1b1c4d03cdcd1c4a36e0eea99bfbeec83be
Cyrill <mazurink@mail.ru>
2021-09-23 01:12:34 +0300

    9.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -20,57 +20,85 @@ public class StartUITest {
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
-                "Menu:" + System.lineSeparator()
-                        + "0. Exit program" + System.lineSeparator()
+                "Menu:" + System.lineSeparator() +
+                        "0. Exit program" + System.lineSeparator()
         ));
     }
 
-    /*@Test
+    @Test
     public void whenCreateItem() {
+        Output out = new StubOutput();
         Input in = new StubInput(
                 new String[] {"0", "Item name", "1"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
+                new CreateAction(out),
                 new ExitAction()
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
+        assertThat(out.toString(), is(
+                "Menu:" + System.lineSeparator()
+                        + "0. Add new Item" + System.lineSeparator()
+                        + "1. Exit program" + System.lineSeparator()
+                        +  "=== Create a new Item ====" + System.lineSeparator()
+                        +  "Menu:" + System.lineSeparator()
+                        +  "0. Add new Item" + System.lineSeparator()
+                        +  "1. Exit program" + System.lineSeparator()
+        ));
     }
 
     @Test
     public void whenReplaceItem() {
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
-        *//* Добавим в tracker новую заявку *//*
         Item item = tracker.add(new Item("Replaced item"));
-        *//* Входные данные должны содержать ID добавленной заявки item.getId() *//*
         String replacedName = "New item name";
         Input in = new StubInput(
                 new String[] {"0", String.valueOf(item.getId()), replacedName, "1"}
         );
         UserAction[] actions = {
-                new ReplaceAction(),
+                new ReplaceAction(out),
                 new ExitAction()
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
+        assertThat(out.toString(), is(
+                "Menu:" + System.lineSeparator()
+                        + "0. Edit item" + System.lineSeparator()
+                        + "1. Exit program" + System.lineSeparator()
+                        +  "=== Edit item ====" + System.lineSeparator()
+                        + "Заявка изменена успешно." + System.lineSeparator()
+                        +  "Menu:" + System.lineSeparator()
+                        +  "0. Edit item" + System.lineSeparator()
+                        +  "1. Exit program" + System.lineSeparator()
+        ));
     }
 
     @Test
     public void whenDeleteItem() {
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
-        *//* Добавим в tracker новую заявку *//*
         Item item = tracker.add(new Item("Deleted item"));
-        *//* Входные данные должны содержать ID добавленной заявки item.getId() *//*
         Input in = new StubInput(
                 new String[] {"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
-                new DeleteAction(),
+                new DeleteAction(out),
                 new ExitAction()
         };
-        new StartUI().init(in, tracker, actions);
+        new StartUI(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
-    }*/
+        assertThat(out.toString(), is(
+                "Menu:" + System.lineSeparator()
+                        + "0. Delete item" + System.lineSeparator()
+                        + "1. Exit program" + System.lineSeparator()
+                        +  "=== Delete item ====" + System.lineSeparator()
+                        + "Заявка удалена успешно." + System.lineSeparator()
+                        +  "Menu:" + System.lineSeparator()
+                        +  "0. Delete item" + System.lineSeparator()
+                        +  "1. Exit program" + System.lineSeparator()
+        ));
+    }
 }
