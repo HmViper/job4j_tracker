5a78a8ae806e917973a188f86dd83ab8d80fc114
Cyrill <mazurink@mail.ru>
2021-09-24 19:49:47 +0300

    9.2. Тесты вывода на консоль [#33585 #207142]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -29,33 +29,24 @@ public class StartUITest {
     public void findAllActionTest() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"0", "new item", "1", "2"}
+                new String[]{"0", "1"}
         );
         Tracker tracker = new Tracker();
+        tracker.add(new Item("test"));
         UserAction[] actions = {
-                new CreateAction(out),
                 new FindAllAction(out),
                 new ExitAction()
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
-
                 "Menu:" + System.lineSeparator()
-                        + "0. Add new Item" + System.lineSeparator()
-                        + "1. Show all items" + System.lineSeparator()
-                        + "2. Exit program" + System.lineSeparator()
-                        + "=== Create a new Item ====" + System.lineSeparator()
-                        + "Menu:" + System.lineSeparator()
-                        + "0. Add new Item" + System.lineSeparator()
-                        + "1. Show all items" + System.lineSeparator()
-                        + "2. Exit program" + System.lineSeparator()
+                        + "0. Show all items" + System.lineSeparator()
+                        + "1. Exit program" + System.lineSeparator()
                         + "=== Show all items ====" + System.lineSeparator()
                         + tracker.findAll()[0] + System.lineSeparator()
                         + "Menu:" + System.lineSeparator()
-                        + "0. Add new Item" + System.lineSeparator()
-                        + "1. Show all items" + System.lineSeparator()
-                        + "2. Exit program" + System.lineSeparator()
-
+                        + "0. Show all items" + System.lineSeparator()
+                        + "1. Exit program" + System.lineSeparator()
         ));
     }
 
@@ -63,33 +54,24 @@ public class StartUITest {
     public void findByNameTest() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"0", "new item", "1", "new item",  "2"}
+                new String[]{"0", "test", "1"}
         );
         Tracker tracker = new Tracker();
+        tracker.add(new Item("test"));
         UserAction[] actions = {
-                new CreateAction(out),
                 new FindItemByNameAction(out),
                 new ExitAction()
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
-
                 "Menu:" + System.lineSeparator()
-                        + "0. Add new Item" + System.lineSeparator()
-                        + "1. Find items by name" + System.lineSeparator()
-                        + "2. Exit program" + System.lineSeparator()
-                        + "=== Create a new Item ====" + System.lineSeparator()
-                        + "Menu:" + System.lineSeparator()
-                        + "0. Add new Item" + System.lineSeparator()
-                        + "1. Find items by name" + System.lineSeparator()
-                        + "2. Exit program" + System.lineSeparator()
+                        + "0. Find items by name" + System.lineSeparator()
+                        + "1. Exit program" + System.lineSeparator()
                         + "=== Find items by name ====" + System.lineSeparator()
                         + tracker.findAll()[0] + System.lineSeparator()
                         + "Menu:" + System.lineSeparator()
-                        + "0. Add new Item" + System.lineSeparator()
-                        + "1. Find items by name" + System.lineSeparator()
-                        + "2. Exit program" + System.lineSeparator()
-
+                        + "0. Find items by name" + System.lineSeparator()
+                        + "1. Exit program" + System.lineSeparator()
         ));
     }
 
@@ -97,33 +79,24 @@ public class StartUITest {
     public void findByIdTest() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"0", "new item", "1", "1",  "2"}
+                new String[]{"0", "1", "1"}
         );
         Tracker tracker = new Tracker();
+        tracker.add(new Item("test"));
         UserAction[] actions = {
-                new CreateAction(out),
                 new FindItemByIdAction(out),
                 new ExitAction()
         };
         new StartUI(out).init(in, tracker, actions);
         assertThat(out.toString(), is(
-
                 "Menu:" + System.lineSeparator()
-                        + "0. Add new Item" + System.lineSeparator()
-                        + "1. Find item by id" + System.lineSeparator()
-                        + "2. Exit program" + System.lineSeparator()
-                        + "=== Create a new Item ====" + System.lineSeparator()
-                        + "Menu:" + System.lineSeparator()
-                        + "0. Add new Item" + System.lineSeparator()
-                        + "1. Find item by id" + System.lineSeparator()
-                        + "2. Exit program" + System.lineSeparator()
+                        + "0. Find item by id" + System.lineSeparator()
+                        + "1. Exit program" + System.lineSeparator()
                         + "=== Find item by id ====" + System.lineSeparator()
                         + tracker.findAll()[0] + System.lineSeparator()
                         + "Menu:" + System.lineSeparator()
-                        + "0. Add new Item" + System.lineSeparator()
-                        + "1. Find item by id" + System.lineSeparator()
-                        + "2. Exit program" + System.lineSeparator()
-
+                        + "0. Find item by id" + System.lineSeparator()
+                        + "1. Exit program" + System.lineSeparator()
         ));
     }
 
