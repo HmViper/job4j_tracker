012eb1570c2fa2ebd6364be9ed9d4a70c5bd8ba4
Cyrill <mazurink@mail.ru>
2021-09-25 22:41:04 +0300

    0.4. Пользовательские исключения. [#219367 #207960]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -15,6 +15,7 @@ public class FindEl {
         for (int index = 0; index < value.length; index++) {
             if (value[index].equals(key)) {
                 rsl = index;
+                break;
             }
 
         }
