71b903d772846cd5495abebab4dede0cf3d7f919
Cyrill <mazurink@mail.ru>
2021-10-14 09:56:16 +0300

    1. Паспорт и жители [#212656 #215708]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -16,13 +16,5 @@ public class PassportOfficeTest {
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
 
-    @Test
-    public void whenAddDuplicate() {
-        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
-        Citizen citizen1 = new Citizen("2f44a", "Petr Arsentev1");
-        PassportOffice office = new PassportOffice();
-        office.add(citizen);
-        office.add(citizen1);
-        Assert.assertFalse(office.add(citizen1));
-    }
+
 }
