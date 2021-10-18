fcc76680f5320bfbd7dd9857b5f0de5f4a1008f5
Cyrill <mazurink@mail.ru>
2021-10-18 21:48:54 +0300

    0. Сортировка [#10096]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Item {
     private int id;
@@ -9,6 +10,19 @@ public class Item {
     private LocalDateTime created = LocalDateTime.now();
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id && name.equals(item.name) && created.equals(item.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, created);
+    }
 
     public Item() {
 
--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -4,6 +4,8 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
@@ -83,4 +85,70 @@ public class TrackerTest {
         tracker.delete(id);
         assertThat(tracker.findById(id), is(nullValue()));
     }
+
+    @Test
+    public void whenSortingAscendingById() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        Item third = new Item("Third");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        List<Item> result = new ArrayList<Item>(tracker.findAll());
+        Collections.sort(result, new SortByIdAsc());
+        List<Item> expected = Arrays.asList(new Item("First", 1),
+                new Item("Second", 2), new Item("Third", 3));
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSortingDescendingById() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        Item third = new Item("Third");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        List<Item> result = new ArrayList<Item>(tracker.findAll());
+        Collections.sort(result, new SortByIdDesc());
+        List<Item> expected = Arrays.asList(new Item("Third", 3),
+                new Item("Second", 2), new Item("First", 1));
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSortingAscendingByName() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("One");
+        Item second = new Item("Two");
+        Item third = new Item("Three");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        List<Item> result = new ArrayList<Item>(tracker.findAll());
+        Collections.sort(result, new SortByNameAsc());
+        List<Item> expected = Arrays.asList(new Item("One", 1),
+                new Item("Three", 3), new Item("Two", 2));
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSortingDescendingByName() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("One");
+        Item second = new Item("Two");
+        Item third = new Item("Three");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        List<Item> result = new ArrayList<Item>(tracker.findAll());
+        Collections.sort(result, new SortByNameDesc());
+        List<Item> expected = Arrays.asList(new Item("Two", 2),
+                new Item("Three", 3), new Item("One", 1));
+        assertThat(result, is(expected));
+    }
+
+
 }
