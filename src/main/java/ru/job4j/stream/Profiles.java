7b31f0d093d1ee6d36e0d7df0bdb9f156cf937f1
Cyrill <mazurink@mail.ru>
2022-01-14 01:30:34 +0300

    2. Список адресов [#110062 #250637]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -12,14 +12,4 @@ public class Profiles {
                 .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
-
-    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        Comparator<Address> addressComparator =
-                Comparator.comparing(Address::getCity);
-        return profiles.stream()
-                .map(Profile::getAddress)
-                .sorted(addressComparator)
-                .distinct()
-                .collect(Collectors.toList());
-    }
 }
